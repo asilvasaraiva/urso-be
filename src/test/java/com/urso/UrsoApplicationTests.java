6381edd96b97df8a9d9c376a894f088804c8e6c4
pve_asaraiva <pve_asaraiva@uolinc.com>
2021-09-04 19:19:54 -0300

    first commit

---
+++ src/test/java/com/urso/UrsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
