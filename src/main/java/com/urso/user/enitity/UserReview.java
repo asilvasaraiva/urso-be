788e5a10a33cb43c205406fb02317929a8065bc5
pve_asaraiva <pve_asaraiva@uolinc.com>
2021-09-04 20:59:12 -0300

    adicionando UserReview[entity]

--- src/main/java/com/urso/user/enitity/UserReview.java
+++ src/main/java/com/urso/user/enitity/UserReview.java
@@ -1,4 +1,18 @@
 package com.urso.user.enitity;
 
-public class UserReview {
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class UserReview implements Serializable {
+
+    private static final long serialVersionUID = -3788024303917538267L;
+    private final long idReview;
+    private final long idPersonSender;
+    private final long idPersonReceiver;
+    private final boolean status;
+    private final LocalDateTime createdAt;
+    private final String content;
 }
