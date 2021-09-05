f3908d4f426c84b6709c7ad003c934b2a25a4397
pve_asaraiva <pve_asaraiva@uolinc.com>
2021-09-05 12:34:11 -0300

    Adicionando mapeamento entidade AdminMessage

--- src/main/java/com/urso/admin/entity/AdminMessage.java
+++ src/main/java/com/urso/admin/entity/AdminMessage.java
@@ -1,18 +1,37 @@
 package com.urso.admin.entity;
 
+import com.urso.user.entity.User;
+import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Builder
+@Table(name = "admin_message")
 public class AdminMessage implements Serializable {
     private static final long serialVersionUID = 5456822156544118281L;
 
-    private final long idAdminMessage;
-    private final long idUser;
+    @Id
+    @Column(name = "id_message")
+    private final long idMessage;
+
+    @Column(name = "id_user_sender")
+    private final long idUserSender;
+
+    @Column(name = "is_read")
     private final boolean isRead = false;
+
+    @Column(name = "dat_creation")
     private final LocalDateTime createAt;
+
+    @Column(name = "content")
     private final String content;
 
 
